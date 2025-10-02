6253d3c10d37a44ce1868e1e97fc715282ec0e16
bliis-hood <2401727@students.kcau.ac.ke>
2025-10-02 15:49:41 +0300

    innit

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,13 +1,30 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
+        <title>CAR-LOG</title>
+        <link rel="stylesheet" href="{{ asset('style.css') }}">
      @include('inc.navbar')
-        <div class="topnav"></div>
-    <div class="jumbotron text-center">
+        <div class="topnav">
         <h1>Discover The Future With Us</h1>
         <p>It is where you discover new things and put new things</p>
-        <div class="link-container">
-        <a href="{{ route('register.form') }}" class="link register-link">Register</a>
-        <a href="{{ route('login.form') }}" class="link login-link">Login</a>
         </div>
-@endsection
+    <body>
+        <div class="relative">
+            @if (Route::has('login'))
+                <div class="sm">
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="dash">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="on">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="reg">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+        </div>   
+    </body>        
--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -25,7 +25,6 @@
         @csrf
         <input type="text" name="name" placeholder="Name" value="{{ old('name') }}" required>
         <input type="email" name="email" placeholder="Email" value="{{ old('email') }}" required>
-        <input type="text" name="phone" placeholder="Phone Number" value="{{ old('phone') }}" required>
         <input type="password" name="password" placeholder="Password" required>
         <input type="password" name="password_confirmation" placeholder="Confirm Password" required>
         <button type="submit" class="submit-btn">Register</button>
